95baf5b35ad39b753013956b33806ebf9ec546c7
Nick Kuizenga <nickkuis@hotmail.com>
2018-07-28 14:27:49 -0400

    Changed settings.py to work with Django, first attempt

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Activate Django-Heorku
+django_heroku.settings(locals())
+
