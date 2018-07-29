eb34ade36090f6ae24ed53e66bfb308d891224e6
Nick Kuizenga <nickkuis@hotmail.com>
2018-07-29 15:07:25 -0400

    Added missing module to views.py file

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+
 from .models import Choice, Question
 
 
@@ -24,7 +26,7 @@ def results(request, question_id):
 	return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-	question = get_object_or_404(Question, pk=question_id):
+	question = get_object_or_404(Question, pk=question_id)
 	try:
 		selected_choice = question.choice_set.get(pk=request.POST['choice'])
 	except (KeyError, Choice.DoesNotExist):
