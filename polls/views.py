482d579948796c4ab0c9de1d71b865b48a58fe9a
Nick Kuizenga <nickkuis@hotmail.com>
2018-07-29 14:53:47 -0400

    First part of Django tutorial Part 4 - testing on Heroku

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import get_object_or_404, render
-
 # Create your views here.
 
-from django.http import HttpResponse
-
-from .models import Question
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResposnseRedirect, HttpResponse
+from .models import Choice, Question
 
 
 def index(request):
@@ -22,11 +20,27 @@ def detail(request, question_id):
 	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the question voting form.
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "You didn't select a choice.",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		# Always return an HttpResponseRedirect after successfully deailing
+		# with POST data. This presents data from being posted twice if a
+		# user hits the Back button.
+		return HttpResposnseRedirect(reverse('polls:results', args=(question.id,)))
+
 
 
 
